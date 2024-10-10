db8c22df2d2acbe77eef78337ad6a1b77fc2b66b
Anish Ghimire <anishghimire50@gmail.com>
2024-10-10 16:24:19 +0400

    added templates and logic to app

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 //SERVER
 const http = require("http");
+const fs = require("fs");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplates");
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   //Creating Routes using if/else statements
-  if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the overview");
-  } else if (pathName === "/product") {
-    res.end("This is the product");
+
+  //Overview
+  if (pathname === "/" || pathname === "/overview") {
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(output);
+
+    //Product
+  } else if (pathname === "/product") {
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(output);
+
+    //Api
+  } else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+
+    //Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
